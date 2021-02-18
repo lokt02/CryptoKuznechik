224c95706bc712a1df2a273ba29036e25bca8710
KostylevVadim <70564584+KostylevVadim@users.noreply.github.com>
2021-02-18 20:21:16 +0300

    Update Byte.ts
    чуть чуть покомментировал

--- typescript/Byte.ts
+++ typescript/Byte.ts
@@ -1,7 +1,7 @@
 export class Byte {
-    value: string;
-    size: number = 0;
-  
+    value: string; //непосредственно значение.
+    size: number = 0; //длина двоичнойзаписи 
+  //конструктор
     constructor(value: number, size: number) {
       let temp: string = value.toString(2);
       if(size === null || size < temp.length){
@@ -10,14 +10,14 @@ export class Byte {
       else{
           this.size = size;
       }
-
+//создание нулей для того, чтобы все длины были одинаковыми
       let dsize: number = size - temp.length
       for(let i = 0; i < dsize; i++){
         temp = '0' + temp;
       }
       this.value = temp;
     }
-
+//вывод значения в десятичном виде как число
     GetValueDec(){
         // let temp: string = this.value.toString(10);
         // return +temp;
@@ -28,7 +28,7 @@ export class Byte {
         }
         return result;
     }
-
+//вывод значения ввиде строки
     GetValueString(){
         return this.value;
     }
@@ -36,16 +36,34 @@ export class Byte {
   }
 
 
-
+//людской ксор
 export function XOR(a: Byte, b: Byte){
-    let max: number = Math.max(a.size, b.size);
-    let result: string = "";
+    let max: number = Math.max(a.size, b.size);//максимвльна длина байтов
+    let result: string = "";//результат сксоривания байтов
     for(let i: number = 0; i < max; i++){
-        let temp: number;
-        temp = +a.GetValueString()[i] + +b.GetValueString()[i];
-        temp = temp % 2;
-        result += temp;
+        let temp: number;//временное число, которое будет запихиваться в итог
+        temp = +a.GetValueString()[i] + +b.GetValueString()[i];//сложение знаков бита
+        temp = temp % 2;//запиливание из простого сложения в побитовое сложение
+        result += temp;//припихивание
     }
 
     return result;
+}
+
+export function Bit_Mult(a: Byte, b:Byte){ 
+
+//Перевод чисел в строки(не массивы для красоты)
+let x:string = a.GetValueString(); 
+let y:string = b.GetValueString(); 
+
+let m:number=Math.max(x.length,y.length); //длины
+let res: string=""; //результат перемножения
+for(let i:number=0;i<m;i++){ 
+let t:number; // итоговое число для запихивания
+t=(+x[i])*(+y[i]); //перемножение
+res+=t; //припихивание к результату
+
+} 
+return res; 
+
 }
