f1724e20f75d0a8cbc505bbdb43d6a426bdbaad9
lokt02 <lokt02@mail.ru>
2021-04-04 19:34:46 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,10 +3,27 @@ import {Kuznec, HexInput} from './kuznec';
 var kuz:Kuznec = new Kuznec();
 kuz.KeyGen(HexInput("7766554433221100FFEEDDCCBBAA9988EFCDAB89674523011032547698BADCFE"));
 
-var buffer: Buffer = Buffer.from('kekw', 'utf-8');
-var bufferTemp: Buffer = Buffer.alloc(16 - buffer.length);
-buffer = Buffer.concat([bufferTemp, buffer]);
-var encrypted = kuz.Encryption(buffer);
-console.log(encrypted);
-var decrypted = kuz.Decryption(encrypted);
-console.log(decrypted.toString('utf-8'));
+const fs = require('fs');
+let inputString: string = fs.readFileSync("input.txt", "utf8");
+let block: string[] = [];
+for(let i = 0; i < inputString.length; i+=16){
+    block.push(inputString.slice(i, i + 15));
+}
+
+var encrypted: Buffer[] = []
+for(let i = 0; i < block.length; i++){
+    var buffer: Buffer = Buffer.from(block[i], 'utf-8');
+    encrypted.push(kuz.Encryption(buffer));
+    //console.log(encrypted);
+}
+
+let decrypted: Buffer[] = [];
+for(let i = 0; i < encrypted.length; i++){
+    decrypted.push(kuz.Decryption(encrypted[i]));
+    // console.log(decrypted.toString('utf-8'));
+}
+let result: string = '';
+for(let i = 0; i < decrypted.length; i++){
+    result += decrypted[i].toString('utf-8');
+}
+console.log(result);
