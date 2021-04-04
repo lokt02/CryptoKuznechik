49e064a6e9bfb6b29f039bf5f970a29e2934fe4a
lokt02 <lokt02@mail.ru>
2021-04-04 16:04:12 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -4,9 +4,9 @@ var kuz:Kuznec = new Kuznec();
 kuz.KeyGen(HexInput("7766554433221100FFEEDDCCBBAA9988EFCDAB89674523011032547698BADCFE"));
 
 var buffer: Buffer = Buffer.from('kekw', 'utf-8');
-console.log(buffer)
-buffer = Buffer.concat([buffer, Buffer.from([32])]);
-console.log(buffer)
-console.log(typeof(buffer[0]))
-
-console.log(HexInput('77 66 55 44 33 22'))
+var bufferTemp: Buffer = Buffer.alloc(16 - buffer.length);
+buffer = Buffer.concat([bufferTemp, buffer]);
+var encrypted = kuz.Encryption(buffer);
+console.log(encrypted);
+var decrypted = kuz.Decryption(encrypted);
+console.log(decrypted.toString('utf-8'));
