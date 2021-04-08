6691b6d050c5bbef6653cb1b01fbfa27afa59d3b
KostylevVadim <70564584+KostylevVadim@users.noreply.github.com>
2021-04-08 22:36:41 +0300

    борьба с ошибками и почти готовность

--- src/СFB.ts
+++ src/СFB.ts
@@ -8,17 +8,20 @@ let key: string='7766554433221100FFEEDDCCBBAA9988EFCDAB89674523011032547698BADCF
 kuz.KeyGen(HexInput(key));
 let enterbuf: Buffer = Buffer.from(entstri);
 let ctr : Buffer =Buffer.alloc(initv.length);
-let numbl: number = entstri.length/32;
-let gamma: Buffer= Buffer.alloc(32);
+let numbl: number = entstri.length/16;
+let gamma: Buffer= Buffer.alloc(16);
 let out : Buffer=Buffer.alloc(entstri.length);
-
 ctr=HexInput(initv.slice());
+console.log(ctr);
+console.log(initv);
+
+
 for(let i:number = 0; i< numbl; i++){
     gamma = kuz.Encryption(ctr);
-    let temp:Buffer = Buffer.alloc(32);
-    for(let j:number = 0; j<32;j++){
-        out[32*i+j]=gamma[j]^enterbuf[32*i+j];
-        temp[j]=out[32*i+j];
+    let temp:Buffer = Buffer.alloc(16);
+    for(let j:number = 0; j<16;j++){
+        out[16*i+j]=gamma[j]^enterbuf[16*i+j];
+        temp[j]=out[16*i+j];
     }
     ctr=temp.slice();
 }
@@ -26,16 +29,17 @@ for(let i:number = 0; i< numbl; i++){
 console.log(enterbuf);
 console.log(out);
 
-ctr=HexInput(initv.slice(0, 32));
+
+ctr=HexInput(initv.slice());
 let dec:Buffer = Buffer.alloc(out.length);
-ctr=HexInput(initv.slice(0, 32));
 for(let i:number = 0; i< numbl; i++){
     gamma = kuz.Encryption(ctr);
-    let temp:Buffer = Buffer.alloc(32);
-    for(let j:number = 0; j<32;j++){
-        dec[32*i+j]=gamma[j]^out[32*i+j];
-        temp[j]=out[32*i+j];
+    let temp:Buffer = Buffer.alloc(16);
+    for(let j:number = 0; j<16;j++){
+        dec[16*i+j]=gamma[j]^out[16*i+j];
+        temp[j]=out[16*i+j];
     }
     ctr=temp.slice();
 }
+
 console.log(dec);
