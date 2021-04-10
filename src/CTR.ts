3ea5dc4051286f9540aaf2cc931c6bc81307c22a
KostylevVadim <70564584+KostylevVadim@users.noreply.github.com>
2021-04-10 23:24:25 +0300

    сделал увеличение ctr до конкретного числа

--- src/CTR.ts
+++ src/CTR.ts
@@ -11,6 +11,17 @@ export class CTR{
             this.initv[i] = Math.floor(Math.random() * 255);
         }
     }
+    NewCTR(ctr: Buffer, x: number){
+        let n:number = x;
+        var arr = n.toString(16).replace(/\D/g, '0').split('').map(Number);
+        while(arr.length!=8){arr.unshift(0);}
+        let temp :Buffer= Buffer.from(arr);
+        for(let i: number=0; i<8; ++i){
+            ctr[i+8]+=temp[i];
+        }
+        return ctr;
+
+    }
     Encrypt(entstri: Buffer){
     }
     
