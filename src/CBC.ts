85fe5578862c3016a2c93f29e91c72646fcb5b00
KostylevVadim <70564584+KostylevVadim@users.noreply.github.com>
2021-04-10 10:54:54 +0300

    уже лучше, но всё ещё не то

--- src/CBC.ts
+++ src/CBC.ts
@@ -1,4 +1,4 @@
-import { Kuznec,HexInput} from './kuznec';
+import { Kuznec} from './kuznec';
 
 export class CBC{
     kuz: Kuznec;
@@ -18,7 +18,7 @@ export class CBC{
         let ctr : Buffer =Buffer.alloc(this.initv.length);
         let numbl: number = entstri.length/16;
         let gamma: Buffer= Buffer.alloc(16);
-        let out : Buffer = Buffer.alloc(0);
+        let out : Buffer=Buffer.alloc(entstri.length);
         ctr=this.initv.slice();
     
         for(let i:number = 0; i< numbl; i++){
@@ -29,8 +29,9 @@ export class CBC{
         }
         temp=this.kuz.Encryption(temp);
         ctr=temp.slice();
-        let l=temp.length+out.length;
-        out = Buffer.concat([out,temp],l)
+        for(let j:number = 0; j<16;j++){
+            out[16*i+j]=temp[j];
+        }           
         }
         if(entstri.length%16!=0){
             let temp:Buffer = Buffer.alloc(entstri.length%16);
@@ -39,40 +40,48 @@ export class CBC{
             }
             temp=this.kuz.Encryption(temp);
             ctr=temp.slice();
-            let l=temp.length+out.length;
-            out = Buffer.concat([out,temp],l)
+            for(let j:number = 0; j<entstri.length%16;j++){
+                out[16*numbl+j]=temp[j];
+            }
     }
     
     return out;
     }
     
-    Decrypt(out:Buffer){
-                
+    Decrypt(entstri:Buffer){
+        
+        this.kuz.KeyGen();
+    
         let ctr : Buffer =Buffer.alloc(this.initv.length);
-        ctr=this.initv.slice();
-        let numbl: number = out.length/16;
-        let dec:Buffer = Buffer.alloc(0);
+        let numbl: number = entstri.length/16;
         let gamma: Buffer= Buffer.alloc(16);
+        let out : Buffer=Buffer.alloc(entstri.length);
+        ctr=this.initv.slice();
+    
         for(let i:number = 0; i< numbl; i++){
-        gamma = this.kuz.Encryption(ctr);
+        //gamma = this.kuz.Encryption(ctr);
         let temp:Buffer = Buffer.alloc(16);
         for(let j:number = 0; j<16;j++){
-            temp[j]=ctr[j]^out[16*i+j];
+            temp[j]=ctr[j]^entstri[16*i+j];
         }
         temp=this.kuz.Encryption(temp);
         ctr=temp.slice();
-        let l=temp.length+dec.length;
-        dec = Buffer.concat([dec,temp],l)
-    }
-    if(out.length%16!=0){
-        let temp:Buffer = Buffer.alloc(out.length%16);
-        for(let j:number = 0; j<out.length%16;j++){
-            temp[j]=ctr[j]^out[16*numbl+j];
+        for(let j:number = 0; j<16;j++){
+            out[16*i+j]=temp[j];
+        }           
         }
-        temp=this.kuz.Encryption(temp);
-        let l=temp.length+dec.length;
-        dec = Buffer.concat([dec,temp],l)
+        if(entstri.length%16!=0){
+            let temp:Buffer = Buffer.alloc(entstri.length%16);
+            for(let j:number = 0; j<entstri.length%16;j++){
+                temp[j]=ctr[j]^entstri[16*numbl+j];
+            }
+            temp=this.kuz.Encryption(temp);
+            ctr=temp.slice();
+            for(let j:number = 0; j<entstri.length%16;j++){
+                out[16*numbl+j]=temp[j];
+            }
     }
-    return dec;
+    
+    return out;
     }
 }
