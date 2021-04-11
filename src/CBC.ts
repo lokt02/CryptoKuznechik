fa4f854d31e41aadbec4f7cfb2df215d1f547a44
KostylevVadim <70564584+KostylevVadim@users.noreply.github.com>
2021-04-11 11:55:27 +0300

    недорабочий CBC

--- src/CBC.ts
+++ src/CBC.ts
@@ -12,20 +12,20 @@ export class CBC{
         }
     }
     Encrypt(entstri: Buffer){
-        let numbl = entstri.length/16;
+        let numbl:number = Math.floor(entstri.length/16);
         let out: Buffer = Buffer.alloc(entstri.length);
-        let tempv: Buffer = Buffer.from(this.initv);
-        
+        let tempv: Buffer = Buffer.from(this.initv);       
         for(let j = 0; j<numbl;j++){
             let temp=Buffer.alloc(16);
             for(let i: number =0; i<16;i++){
                 temp[i]=entstri[j*16+i];
             }
+            
             for(let i: number =0; i<16;i++){
                 temp[i]=temp[i]^tempv[i];
             }
             temp=this.kuz.Encryption(temp);
-            tempv= Buffer.from(temp)
+            tempv= Buffer.from(temp);
             for(let i: number =0;i<16;i++){
                 out[16*j+i]=temp[i];
             }
@@ -48,25 +48,28 @@ export class CBC{
     }
     
     Decrypt(entstri:Buffer){
-        let numbl = entstri.length/16;
+        let numbl:number = Math.floor(entstri.length/16);
         let out: Buffer = Buffer.alloc(entstri.length);
         let tempv: Buffer = Buffer.from(this.initv);
         for(let j: number= 0;j< numbl;j++){
-            
             let temp:Buffer= Buffer.alloc(16);
+            let temp1:Buffer= Buffer.alloc(16);
             for(let i: number =0;i<16;i++){
                 temp[i]=entstri[j*16+i];
+                temp1[i]=entstri[j*16+i];
             }
+            
             temp=this.kuz.Decryption(temp);
             for(let i: number=0;i<16;i++){
                 temp[i]=temp[i]^tempv[i];
             }
+            
             for(let i: number=0;i<16;i++){
                 out[16*j+i]=temp[i];
             }
+            tempv = Buffer.from(temp1);
         }
         if(entstri.length%16!=0){
-            
             let temp:Buffer= Buffer.alloc(entstri.length%16);
             for(let i: number =0;i<entstri.length%16;i++){
                 temp[i]=entstri[numbl*16+i];
